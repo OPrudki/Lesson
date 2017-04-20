8c3c111853d2d5197a348262950625500effc076
Oleg Prudki <oleg.sales@gmail.com>
2017-04-20 16:00:11 +0300

    Main.java

--- Lesson3/src/Main.java
+++ Lesson3/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
   System.out.println("Hello " + args[0]);
+  // стейтмент - это утверждение
+  // выражение - это конструкция, в результате которй получается значение
   }
 }
