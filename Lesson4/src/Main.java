3ed0095683b09d464dca2375edc95926190dade1
Oleg Prudki <oleg.sales@gmail.com>
2017-04-20 20:44:12 +0300

    .

---
+++ Lesson4/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+  public static void main(String[] args) {
+  System.out.println("WoW");
+
+System.out.println(a);
+System.out.println(c);
+System.out.println(d);
+System.out.println(f);
+System.out.println(l);
+
+double localD = 10.0d;
+
+System.out.println(localD);
+
+String str = new String ("Hello");
+String str2 = "foo bar";
+
+System.out.println(str);
+
+  }
+}
